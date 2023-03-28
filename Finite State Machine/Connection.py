3be3e9c58f7f4562366bbf15355f6a8239252485
Jeff-Andrade <80226537+Jeff-Andrade@users.noreply.github.com>
2023-03-28 19:51:40 -0300

    Delete Finite State Machine directory

--- Finite State Machine/Connection.py
+++
@@ -1,6 +0,0 @@
-class Connection:
-  def __init__(self, next, char):
-    self.char = char
-    self.next = next
-  def compare_input(self, input):
-    return self.char == input
--- Finite State Machine/Main.py
+++
@@ -1,7 +0,0 @@
-from Parser import Parser
-
-parser = Parser("input-example.txt")
-automaton = parser.get_automaton()
-
-print(automaton.accepts_word("abbabbaaa"))
-automaton.print_path()
