0abff250aebdefe43031f6a0f7ce8cc7b54f60da
Jeff-Andrade <80226537+Jeff-Andrade@users.noreply.github.com>
2023-03-28 19:59:40 -0300

    Update Parser.py
    Solved issues with automaton input in text file

--- Parser.py
+++ Parser.py
@@ -29,7 +29,7 @@ class Parser:
 
         dic = {}
 
-        fileData = fileData.split("# ")
+        fileData = fileData.split("#")
         fileData.pop(0)
 
         for i in range(len(fileData)):
