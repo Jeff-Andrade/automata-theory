18a26c4bf99ffd190cc746282515e26088ebb341
Jeff-Andrade <80226537+Jeff-Andrade@users.noreply.github.com>
2023-03-29 10:00:48 -0300

    Add files via upload

--- Automaton.py
+++ Automaton.py
@@ -12,11 +12,15 @@ class Automaton:
     def accepts_word(self, code):
         self.path.clear()
         self.current = self.start
+        self.path.append(self.start.name)
         for char in code:
             self.choose_path(char)
             self.path.append(self.current.name)
         return self.current.isAccepting
 
     def print_path(self):
-        for node in self.path:
-            print(node)
+        for i in range(len(self.path)):
+            print(self.path[i], end="")
+            print(" -> ", end="")
+            if i == len(self.path) - 1:
+                print(self.path[i], end="")
--- Main.py
+++ Main.py
@@ -3,5 +3,5 @@ from Parser import Parser
 parser = Parser("input-example.txt")
 automaton = parser.get_automaton()
 
-print(automaton.accepts_word("ccaaccaa"))
+print(automaton.accepts_word("acacbbcbaacccccbcbab"))
 automaton.print_path()
